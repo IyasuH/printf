2de5252b52c527f034d7baaad91ac71c89552b71
root <IyasuH>
2021-10-05 13:56:04 -0700

    update

--- printf_01.c
+++ printf_01.c
@@ -9,30 +9,30 @@
  */
 int _printf(const char *format, ...)
 {
-
 const char *traverse;
 unsigned int i;
 char *s;
 int length = 0;
-
 va_list arg;
 va_start(arg, format);
-for(traverse = format; *traverse != '\0'; traverse++)
+for (traverse = format; *traverse != '\0'; traverse++)
 {
-while(*traverse != '%')
+while (*traverse != '%')
 {
 putchar(*traverse);
 traverse++;
 continue;
 }
 traverse++;
-switch(*traverse)
+switch (*traverse)
 {
-case 'c' : i = va_arg(arg,int);
+case 'c':
+i = va_arg(arg, int);
 printf("%c", i);
 ++length;
 break;
-case 's': s = va_arg(arg, char *);
+case 's':
+s = va_arg(arg, char *);
 printf("%s", s);
 length += strlen(s);
 break;
@@ -42,5 +42,5 @@ puts("%%");
 }
 }
 va_end(arg);
-return(length);
+return (length);
 }
